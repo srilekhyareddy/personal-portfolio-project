cc952bedd828d45e3db88bcb304c16381ab7e158
Srilekhya Meda <srilekhyareddy@gmail.com>
2022-04-17 13:45:53 -0700

    Created the home page and blog page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def all_blogs(request):
+	return render(request, 'blog/all_blogs.html')
+
+	
+
